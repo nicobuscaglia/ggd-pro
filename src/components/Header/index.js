cbac8f3ebc8571fdc3a63064b7c05f9f3d9b3626
Nicolas Buscaglia <nicolas@gogetdoc.com>
2022-04-12 18:13:51 -0300

    [GGD-2335] - Create Header with Logo (reuse) and ProviderHeader components.

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import { Box } from "@material-ui/core";
+import { Logo } from "../Logo";
+
+const Header = () => {
+  return (
+    <Box>
+      <Logo height="25px" />
+    </Box>
+  );
+};
+
+export { Header };
